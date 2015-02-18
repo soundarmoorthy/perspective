3b4fc915cd387a14f05b7c25bf29af7e953c954a
Soundararajan Dhakshinamoorthy <er.soundararajan@gmail.com>
2015-02-18 08:08:27 +0530

    Setup local sensors ready for app engine integration

--- app/src/main/java/com/freescale/sensors/sfusion/LocalSensors.java
+++ app/src/main/java/com/freescale/sensors/sfusion/LocalSensors.java
@@ -63,8 +63,8 @@ class LocalSensors extends SensorsWrapper {
         mag.setName("Magnetometer");
         gyro.setName("Gyroscope");
         localSensorManager = (SensorManager) demo.getSystemService(Context.SENSOR_SERVICE);
-        setDescriptions();
     }
+
 
     public synchronized void updateRotation(long time, float[] values) {
         SensorManager.getQuaternionFromVector(workingQuaternion, values);
@@ -96,44 +96,8 @@ class LocalSensors extends SensorsWrapper {
         return (localGyro != null);
     }
 
-    private void checkGyroAvailability(Sensor local_gyro) {
-        String msg;
-        if ((local_gyro == null) && (demo.dataSource == DataSource.LOCAL)) {
-            String title = "Warning!";
-            msg = "This Android device does not include a gyroscope.";
-            FlicqUtils.popupAlert(title, msg);
-        }
-    }
-
     public synchronized void run() {
         run(true);
-    }
-
-    private int rateLookup(String lbl, int idx, boolean writeRates) {
-        int retVal = SensorManager.SENSOR_DELAY_NORMAL;
-        String rateString = new String();
-        switch (idx) { // This order needs to match defined in strings.xml
-            case 0:
-                retVal = SensorManager.SENSOR_DELAY_NORMAL;
-                rateString = " sampling at SENSOR_DELAY_NORMAL\n";
-                break;
-            case 1:
-                retVal = SensorManager.SENSOR_DELAY_UI;
-                rateString = " sampling at SENSOR_DELAY_UI\n";
-                break;
-            case 2:
-                retVal = SensorManager.SENSOR_DELAY_GAME;
-                rateString = " sampling at SENSOR_DELAY_GAME\n";
-                break;
-            case 3:
-                retVal = SensorManager.SENSOR_DELAY_FASTEST;
-                rateString = " sampling at SENSOR_DELAY_FASTEST\n";
-                break;
-            case 4:
-                retVal = -1;
-                rateString = " sampling OFF\n";
-        }
-        return (retVal);
     }
 
     public void setSensorRate(Sensor sensor, int rate) {
@@ -145,52 +109,22 @@ class LocalSensors extends SensorsWrapper {
 
     @Override
     public void setSensorRateBySensorType(SensorType type, int rate) {
-        switch (type) {
-            case ACCEL:
-                setSensorRate(localAcc, rate);
-                break;
-            case MAG:
-                setSensorRate(localMag, rate);
-                break;
-            case GYRO:
-                setSensorRate(localGyro, rate);
-                break;
-            case QUAT:
-                setSensorRate(localRotationVectorSensor, rate);
-                break;
-        }
     }
 
     public synchronized void run(boolean register_listeners) {
-        checkGyroAvailability(localGyro);  // force 6-axis fusion if no gyro.  Also sets sts field on button row
-        int accRateIndex = demo.myPrefs.getInt("acc_sample_rate", SensorManager.SENSOR_DELAY_NORMAL);
-        int magRateIndex = demo.myPrefs.getInt("mag_sample_rate", SensorManager.SENSOR_DELAY_NORMAL);
-        int gyroRateIndex = demo.myPrefs.getInt("gyro_sample_rate", SensorManager.SENSOR_DELAY_NORMAL);
-        int rotationVectorRateIndex = demo.myPrefs.getInt("rotation_vector_sample_rate", SensorManager.SENSOR_DELAY_NORMAL);
         boolean writeRates = !register_listeners;
-        int accSampleRate = rateLookup("accelerometer", accRateIndex, writeRates);
-        int magSampleRate = rateLookup("magnetometer", magRateIndex, writeRates);
-        int gyroSampleRate = rateLookup("gyro", gyroRateIndex, writeRates);
-        int rotationVectorSampleRate = rateLookup("rotation vector sensor", rotationVectorRateIndex, writeRates);
-
         // Now let's access local sensors
         if (sensorsEnabled == false) {
             localAcc = localSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
             localGyro = localSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
             localMag = localSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-            localPressure = localSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
-            localLight = localSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-            localProximity = localSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-            localTemperature = localSensorManager.getDefaultSensor(Sensor.TYPE_TEMPERATURE);
             localRotationVectorSensor = localSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-            localLinearAccelerometer = localSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-            localOrientation = localSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-            checkGyroAvailability(localGyro);
             if (register_listeners) {
-                setSensorRate(localAcc, accSampleRate);
-                setSensorRate(localMag, magSampleRate);
-                setSensorRate(localGyro, gyroSampleRate);
-                setSensorRate(localRotationVectorSensor, rotationVectorSampleRate);
+                int rate = SensorManager.SENSOR_DELAY_UI;
+                setSensorRate(localAcc, rate);
+                setSensorRate(localMag, rate);
+                setSensorRate(localGyro,rate);
+                setSensorRate(localRotationVectorSensor, rate);
             }
         }
         if (register_listeners) {
@@ -205,47 +139,6 @@ class LocalSensors extends SensorsWrapper {
         }
         sensorsEnabled = false;
     }
-
-    synchronized String getRecord() {
-        String str1, str2, str3;
-        // format statement converts acceleration from m/s^2 to g's
-        str1 = String.format("%10.4f, %5.3f, %5.3f, %5.3f, ", acc.time(), acc.x(), acc.y(), acc.z());
-        str2 = String.format("%10.4f, %5.3f, %5.3f, %5.3f, ", mag.time(), mag.x(), mag.y(), mag.z());
-        str3 = String.format("%10.4f, %5.2f, %5.2f, %5.2f\n", gyro.time(), gyro.x(), gyro.y(), gyro.z());
-        return (str1 + str2 + str3);
-    }
-
-    private String getSensorDescription(Sensor sensor, String type, String units1, String units2, float s) {
-        String str = new String("");
-        if (sensor != null) {
-            str += "Local " + type + " vendor = " + sensor.getVendor() + ".\n";
-            str += "Local " + type + " name = " + sensor.getName() + ".\n";
-            str += "Local " + type + " resolution = " + sensor.getResolution() + " " + units1 + "/LSB.\n";
-            if (units2 != null) {
-                str += "Local " + type + " resolution = " + sensor.getResolution() / s + " " + units2 + "/LSB.\n";
-            }
-            str += "Local " + type + " range = +/-" + sensor.getMaximumRange() + " " + units1 + "\n";
-            if (units2 != null) {
-                str += "Local " + type + " range = +/-" + sensor.getMaximumRange() / s + " " + units2 + "\n";
-            }
-            str += "Local " + type + " current draw = " + sensor.getPower() + " mA.";
-        } else {
-            str += "This device does not include a local " + type + " sensor.";
-        }
-        return (str);
-    }
-
-    private void setDescriptions() {
-        localAcc = localSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        localGyro = localSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        localMag = localSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        localRotationVectorSensor = localSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-        acc.setDescription(getSensorDescription(localAcc, "accelerometer", "m/s^2", "g", (float) g));
-        gyro.setDescription(getSensorDescription(localGyro, "gyro", "radians/sec", "dps", (float) radiansPerDegree));
-        mag.setDescription(getSensorDescription(localMag, "magnetometer", "micro-Teslas", null, 1));
-        quaternion.setDescription(getSensorDescription(localRotationVectorSensor, "rotation vector sensor", "degrees", null, 1));
-    }
-
 
     private final SensorEventListener localSensorListener = new SensorEventListener() {
         @Override
