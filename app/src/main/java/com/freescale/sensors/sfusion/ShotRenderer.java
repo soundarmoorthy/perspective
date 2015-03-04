f7b1a45cd6d24c5edfc4fd84aec21f9fb0eb51b0
Soundararajan Dhakshinamoorthy <er.soundararajan@gmail.com>
2015-03-05 00:30:47 +0530

    Implement line drawing using simple vertex buffer

--- app/src/main/java/com/freescale/sensors/sfusion/ShotRenderer.java
+++ app/src/main/java/com/freescale/sensors/sfusion/ShotRenderer.java
@@ -51,79 +51,32 @@ public class ShotRenderer implements GLSurfaceView.Renderer {
     public ShotRenderer(FlicqActivity activity, int screenRotation, int[] surfaces, float[] dimensions, String desc) {
         this.activity = activity;
         this.screenRotation = screenRotation;
-        this.line = new Line(surfaces, dimensions, desc);
+        this.line = new Line();
     }
 
-    // Call back when the surface is first created or re-created.
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        Line c = null;
-        gl.glClearColor(1.0f, 1.0f, 1.0f, 0.5f); // specify clear values for the color buffer
-        gl.glClearDepthf(1.0f);            // Set depth's clear-value to farthest
-        gl.glEnable(GL10.GL_DEPTH_TEST);   // Enables depth-buffer for hidden surface removal
-        gl.glDepthFunc(GL10.GL_LEQUAL);    // The type of depth testing to do
-        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);  // nice perspective view
-        gl.glDisable(GL10.GL_DITHER);
-
-        line.loadTextures(gl, activity);
     }
 
-    // Call back after onSurfaceCreated() or whenever the window's size changes.
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
-       /* Called when the surface changed size.
-        * Called after the surface is created and whenever the OpenGL ES surface size changes.
-   	 * Typically you will set your viewport here. If your camera is fixed then you could also set your projection matrix here
-   	 */
-        this.screenWidth = width;
-        this.screenHeight = height;
-        gl.glMatrixMode(GL10.GL_PROJECTION); // specify which matrix is the current matrix
-        gl.glLoadIdentity();
-        float ratio = (float) width / height;
-   	
-   	/* GLU.gluPerspective sets up a perspective projection matrix.  Parameters: 
-   	 * gl 	: a GL10 interface 
-   	 * fovy : specifies the field of view angle, in degrees, in the Y direction. 
-   	 * aspect:  specifies the aspect ratio that determines the field of view in the x direction. 
-   	 * 			The aspect ratio is the ratio of x (screenWidth) to y (screenHeight). 
-   	 * zNear: specifies the distance from the viewer to the near clipping plane (always positive). 
-   	 * zFar : specifies the distance from the viewer to the far clipping plane (always positive).  
-   	 */
-        GLU.gluPerspective(gl, 60.0f, ratio, 0.1f, 30.0f);
-   	
-   	/* void glViewport(GLint x, GLint y, GLsizei screenWidth, GLsizei screenHeight)
-   	 * x, y : Specify the lower left corner of the viewport rectangle, in pixels. The initial value is (0, 0).
-   	 * screenWidth, screenHeight : 	Specify the screenWidth and screenHeight of the viewport. When a GL context is first attached to a 
-   	 * 					surface (e.g. window), screenWidth and screenHeight are set to the dimensions of that surface.
-   	 */
+        if(height == 0) {
+            height = 1;
+        }
+        gl.glClearColor(1.0f,1.0f,1.0f,0.5f);
         gl.glViewport(0, 0, width, height);
-
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        gl.glLoadIdentity();
+        GLU.gluPerspective(gl, 45.0f, (float)width / (float)height, 0.1f, 100.0f);
         gl.glMatrixMode(GL10.GL_MODELVIEW);
         gl.glLoadIdentity();
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
-        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);  // clear buffers to preset values
-        gl.glMatrixMode(GL10.GL_MODELVIEW);
-        gl.glLoadIdentity();  // replace the current matrix with the identity matrix
-
-        activity.dataSelector.getData(rv, this.screenRotation);  // screenRotation only affects fixed rotations
-
-        gl.glTranslatef(0.0f,0.0f, 8* line.offset);
-
-        gl.glRotatef(rotationDegrees[this.screenRotation], 0, 0, 1);  // portrait/landscape rotation
-        gl.glRotatef(rv.a ,rv.x,rv.y,rv.z);
-
-        // Do fixed corrections based on portrait/landscape
-        switch (activity.dataSource) {
-            case STOPPED:
-            case FIXED:
-                if (this.screenRotation != 0) {
-                    gl.glRotatef(+90.0f, 0.0f, 0.0f, 1.0f);  // correct for portrait
-                }
-                gl.glTranslatef(0.0f,0.0f, 8 * line.offset );
-        }
+        gl.glClear(GL10.GL_DEPTH_BUFFER_BIT | GL10.GL_COLOR_BUFFER_BIT);
+        gl.glLoadIdentity();
+        gl.glTranslatef(0.0f, 0.0f, -5.0f);
         line.draw(gl);
     }
 }
